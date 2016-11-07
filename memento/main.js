12dfb96ae9a03e2d17b64c655b3fc0a8ca2147e4
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-11-07 20:53:08 +0200

    memento import

---
+++ memento/main.js
@@ -0,0 +1,13 @@
+var path = require('path')
+var util = require('util')
+var async = require('async')
+var fs = require('fs')
+var YAML = require('yamljs')
+
+var isikud = YAML.load('6.2b.yaml')
+
+var logger = require(path.resolve(__dirname, 'logging.js'))('out.log')
+
+logger.log('isikud')
+logger.log(isikud)
+logger.log('posikud')
