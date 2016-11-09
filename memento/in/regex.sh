f5fca525048ab1a95be3f20ea4a9af595959da19
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-11-09 10:26:49 +0200

    memento kuues raamat yaml

--- memento/in/regex.sh
+++ memento/in/regex.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-npm install replace -g
+# npm install replace -g
 
 declare -a arr=("6-2" "6-3" "6-4" "6-5")
+touch memento.txt
+rm memento.txt
 
 for filenumber in "${arr[@]}"
 do
@@ -10,14 +12,13 @@ do
   outfile="$filenumber".txt
   cp "$infile" "$outfile"
 
-  cp "$infile" "$outfile"
   sed 's/\<b\>[aA]rr\. *\<\/b\>/arr\. /g' "$outfile" > 1."$outfile"
   sed 's/\<b\>[kK]üüdit. *\<\/b\>/küüdit. /g' 1."$outfile" > 2."$outfile"
   sed 's/\<b\>[aA]sum. *\<\/b\>/asum. /g' 2."$outfile" > 3."$outfile"
   sed 's/^\<b\>/\<nimi\>/g' 3."$outfile" > 4."$outfile"
 
   tr '\n' ' ' < 4."$outfile" > 5."$outfile"
-  sed $'s/\<nimi\>/\\\n\<nimi\>/g' 5."$outfile" > 6."$outfile"
+  sed $'s/\<nimi\>/\\\n\<nimi\>/g' 5."$outfile" | tail -n +2 > 6."$outfile"
 
   cp 6."$outfile" 7."$outfile"
   replace '<b>(.*?)</b>' '$1' 7."$outfile"
@@ -26,9 +27,12 @@ do
   cp 7."$outfile" 8."$outfile"
   replace '"' '\"' 8."$outfile"
 
-  cp 8."$outfile" "$outfile"
-  replace '</nimi>(((e|i)n\.* )*ka) ([a-z ,õüäö]*), ' '<alias type="$1">$4</alias>' "$outfile"
+  cp 8."$outfile" 9."$outfile"
+  # replace '</nimi>(((e|i)n\.* )*ka) ([a-z ,õüäö]*), ' '<alias type="$1">$4</alias>' 9."$outfile"
 
+  awk '{printf("<mid>'${filenumber}'-%05d</mid>%s\n", NR, $0)}' 9."$outfile" >> m.txt
+
+  rm   "$outfile"
   rm 1."$outfile"
   rm 2."$outfile"
   rm 3."$outfile"
@@ -37,9 +41,17 @@ do
   rm 6."$outfile"
   rm 7."$outfile"
   rm 8."$outfile"
+  rm 9."$outfile"
 
 done
 
+sed $'s/\<mid\>/- memento id: /g' m.txt > m.id.txt
+sed $'s/\<\/mid\>\<nimi\>/\\\n  nimi: /g' m.id.txt > m.nimi.txt
+sed $'s/\<\/nimi\>/\\\n  kirje: /g' m.nimi.txt > memento.yaml
+
+rm m.txt
+rm m.id.txt
+rm m.nimi.txt
 
 # cp 8."$outfile" "$filenumber".json
 # replace '<nimi>(.*)</nimi>(.*)' '{"nimi":"$1","kirje":"$2"},' "$filenumber".json
