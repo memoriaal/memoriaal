54bdc4edfe1a970a66c02804d8d9bee5d421a544
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-11-29 11:33:15 +0200

    sünniaasta ja nimede lahutamine SQLis

---
+++ memento.sql
@@ -0,0 +1,8 @@
+update `x_m6_2016_11_28`
+  set perenimi =  SUBSTRING_INDEX(nimi, ',', 1),
+  eesnimi = SUBSTRING_INDEX(SUBSTRING_INDEX(nimi, ',', 2), ',', -1),
+  isanimi = If(
+    length(nimi) - length(replace(nimi, ',', '')) > 1,
+    SUBSTRING_INDEX(SUBSTRING_INDEX(nimi, ',', 3), ',', -1),
+    NULL
+  )
