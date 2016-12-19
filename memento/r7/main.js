19b9289cf0ba56d6452e2f1b4302dce4ab9d55cb
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2016-12-19 12:44:37 +0200

    memento seitsmes raamat veaparandustega

--- memento/r7/main.js
+++ memento/r7/main.js
@@ -13,11 +13,13 @@ const csvWrite = function csvWrite(isik) {
     +   '"' + ( isik.memento            ? isik.memento             : '' ) + '"'
     + ', "' + ( isik.perenimi           ? isik.perenimi            : '' ) + '"'
     + ', "' + ( isik.eesnimi            ? isik.eesnimi             : '' ) + '"'
-    + ', "' + ( isik['sünd']            ? isik['sünd']             : '0000' ) + '"'
-    + ', "' + ( isik.kasHukkunud        ? 1                        : 0  ) + '"'
+    + ', "' + ( isik['sünniaasta']      ? isik['sünniaasta']       : '0000' ) + '"'
+    + ', "' + ( isik['altSünd']         ? isik['altSünd']          : '' ) + '"'
+    + ', "' + ( isik.kasHukkunud        ? isik.kasHukkunud         : 0  ) + '"'
     + ', "' + ( isik.rviidad            ? isik.rviidad             : '' ) + '"'
     + ', "' + ( isik.kasutamataKirjeosa ? isik.kasutamataKirjeosa  : '' ) + '"'
     + ', "' + ( isik.kirje              ? isik.kirje               : '' ) + '"'
+    + ','   + ( isik.mviide             ? ' "' + isik.mviide.join(',') + '"' : '' ) + ''
     + '\n'
   )
 }
@@ -25,11 +27,13 @@ csvWrite({
   memento: 'memento',
   perenimi: 'perenimi',
   eesnimi: 'eesnimi',
-  sünd: 'sünd',
+  sünniaasta: 'sünniaasta',
+  altSünd: 'altSünd',
   kasHukkunud: 'kasHukkunud',
   rviidad: 'rviidad',
   kasutamataKirjeosa: 'kasutamataKirjeosa',
-  kirje: 'kirje'
+  kirje: 'kirje',
+  mviide: ['mviide']
 })
 
 
@@ -39,9 +43,10 @@ const leftPad = function(i) {
 }
 
 
-var exec = require('child_process').exec;
-var cmd = 'pdftotext r7.pdf -nopgbrk -enc UTF-8';
+var isikud = []
 
+// var exec = require('child_process').exec
+// var cmd = 'pdftotext r7.pdf -nopgbrk -enc UTF-8'
 // exec(cmd, function(error, stdout, stderr) {
   var lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('r7.txt')
@@ -50,7 +55,16 @@ var cmd = 'pdftotext r7.pdf -nopgbrk -enc UTF-8';
   lineReader.on('line', function (line) {
     parse_line(line)
   })
+
+  lineReader.on('close', function (line) {
+    console.log('finished')
+    console.log(isikud.length)
+    post_process(isikud)
+  })
+
 // })
+
+var parandused = YAML.load('parandused.yaml')
 
 const parse_line = function(line) {
   if (line === '') { return }
@@ -64,15 +78,15 @@ const parse_line = function(line) {
   line = line.replace(re, '')
   re = /^5. NIMEDE KOONDREGISTER R1-R6 ?/
   line = line.replace(re, '')
-  line = line.replace('¸', ', ')
-  line = line.replace('\\', '')
-  line = line.replace('. †', ', †')
-  line = line.replace('R1 †', 'R1, †')
-  line = line.replace('R2 †', 'R2, †')
-  line = line.replace('R3 †', 'R3, †')
-  line = line.replace('R1.R3', 'R1,R3')
-  line = line.replace('R6_2', 'R6-2')
-  line = line.replace('?!', '0000')
+  // Poolitusmärke eemaldades kadusid sidekriipsud ka liitnimede seest.
+  // Convert all CamelCaseStrings to Dash-Separated-Strings
+  re = /([A-ZŠ])([A-ZŠ])([a-z])|([a-z])([A-ZŠ])/g
+  line = line.replace(re, '$1$4-$2$3$5')
+  parandused.forEach(function(parandus) {
+    line = line.replace(parandus.f, parandus.t)
+  })
+
+  // line = line.replace('ŽukovVeseltšak', 'Žukov-Veseltšak')
 
   re = /\b([0-9]{1,5}\. )/g
   let linea = line.replace(re, '\n$1').split('\n')
@@ -84,14 +98,21 @@ const parse_line = function(line) {
 }
 
 const parse_record = function(record) {
-  record = record.replace(/^,+|,+$/gm,'')
+  record = record.replace('?!', '0000')
+  record = record.replace(/^[, ]+|[, ]+$/gm,'')
+  record = record.replace(', sünd. 1', ', sünd 1')
+  record = record.replace(' s ka 1', ', sünd 1')
 
   // Split records without numbers that are appended
   if (record.split(/ ?† /).length > 1) {
     // console.log('SPLIT1: ' + record);
     let a = record.split(/ ?† /)
     record = a.shift() + ' †'
-    parse_record('0. ' + a.join(' † '))
+    let next_r = '0. ' + a.join(' † ').replace(/^[, ]+|[, ]+$/gm,'')
+    // if (next_r === '0. R5, †') {
+    //   console.log(record);
+    // }
+    parse_record(next_r)
   }
   let re = /(,? R[0-9\-]+(?:, ?R[0-9\-]+)*(?:, ?†)?)(.*)$/
   let match = re.exec(record)
@@ -101,8 +122,12 @@ const parse_record = function(record) {
   }
   if (match[2] !== '') {
     // console.log({R:record, M:match})
+    let next_r = '0. ' + match[2].replace(/^[, ]+|[, ]+$/gm,'')
+    // if (next_r === '0. R5, †') {
+    //   console.log(record);
+    // }
+    parse_record(next_r)
     record = record.slice(0, match.index) + match[1]
-    parse_record('0. ' + match[2])
   }
 
   let isik = { kirje: record, kasutamataKirjeosa: record }
@@ -112,7 +137,7 @@ const parse_record = function(record) {
     let re = /^([0-9]{1,5})\. /
     let match = re.exec(isik.kasutamataKirjeosa)
     if (match !== null) {
-      isik['memento'] = 'r7-' + leftPad(match[1])
+      isik['memento'] = (match[1] === '0' ? '' : 'r7-' + leftPad(match[1]))
       isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '')
     }
   })(isik)
@@ -137,226 +162,126 @@ const parse_record = function(record) {
     }
   })(isik)
 
-  // Nimi, sünd
+  // Nimi, sünniaasta
   ;((isik) => {
-    let re = /^([^,]+), ?([^,]*), ?([0-9]{2,4})(.*)/
+    let re = /^([^,]+), ?([^,]*), ?([0-9]{2,4}|\?)(.*)/
     let match = re.exec(isik.kasutamataKirjeosa)
     if (match !== null) {
-      isik['perenimi'] = match[1]
-      isik['eesnimi'] = match[2]
-      isik['sünd'] = match[3]
-      isik.kasutamataKirjeosa = match[4]
+      isik['perenimi'] = match[1].trim()
+      isik['eesnimi'] = match[2].trim()
+      isik['sünniaasta'] = (match[3] === '?' ? '0000' : match[3])
+      isik.kasutamataKirjeosa = match[4].replace(/^[, ]+|[, ]+$/gm,'')
     }
   })(isik)
 
-  csvWrite(isik)
+  // Sünd - alternatiivne sünniinfo
+  ;((isik) => {
+    let re = /(?:sünd ([0-9]{1,4}))/
+    let match = re.exec(isik.kasutamataKirjeosa)
+    if (match !== null) {
+      isik['altSünd'] = match[1]
+      isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '').replace(/^[, ]+|[, ]+$/gm,'')
+    }
+  })(isik)
+
+  isikud.push(isik)
 }
 
-var isikud = []
-
-// var perekond = isikud[0].memento
-isikud.forEach(function(isik) {
-  isik.kasutamataKirjeosa = '@NIMI@ ' + String(isik.kirje)
-  isik.kirje = isik.nimi + ' ' + String(isik.kirje)
-
-  let perenimi = isik.nimi.split(',')[0]
-  if (perenimi === perenimi.toUpperCase()) {
-    perekond = isik.memento
-  }
-  isik.perekond = perekond
-
-  // Sugulus
-  if (isik.perekond === isik.memento) {
-    isik.sugulus = 'perekonnapea'
-  } else {
-    sugulused.forEach(function(sugulus) {
-      let re = new RegExp('[ ,]' + sugulus + '[ ,]','')
-      if (re.test(isik.kasutamataKirjeosa)) {
-        isik.sugulus = sugulus
-        isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, ' @SUGULUS@ ')
-      }
-    })
-  }
-
-  // Rahvus
-  rahvused.forEach(function(rahvus) {
-    let re = new RegExp('[ ,]' + rahvus + '[ ,]','')
-    if (re.test(isik.kasutamataKirjeosa)) {
-      isik.rahvus = rahvus.toLowerCase()
-      isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, ' @RAHVUS@ ')
-    }
+const post_process = function(isikud) {
+  let viiteid = 0
+  let lahendatud_viiteid = 0
+  let pass1 = true
+  let pass2 = true
+  isikud
+  // Filtreerin välja viitavad isikukirjed
+  .filter(function(isik){
+    let re = /^vt /
+    let match = re.exec(isik.kasutamataKirjeosa)
+    return (match === null ? false : true)
   })
 
-  // mitte küüditatud
-  ;((isik) => {
-    let re = /(mitte küüditatud|küüditamata)/
-    let match = re.exec(isik.kasutamataKirjeosa)
-    if (match !== null) {
-      isik['mitte küüditatud'] = '+'
-      isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@MITTEKÜÜDITATUD@')
+  // Lingin kokku, kus klapib "vt " ja teisalt "ka "
+  // Ülejäänud saadan edasi
+  .filter(function(i1){
+    viiteid ++
+    i1.mviide = []
+    let linked = false
+    let q1 = i1.kasutamataKirjeosa
+    isikud.some(function(i2){
+      if (i2.memento === '') { return false }
+      if (i2.kasutamataKirjeosa === '') { return false }
+      let q2 = i2.kasutamataKirjeosa
+      if (q1.indexOf(i2.eesnimi) !== -1
+      && q1.indexOf(i2.perenimi) !== -1
+      && q2.indexOf(i1.eesnimi) !== -1
+      && q2.indexOf(i1.perenimi) !== -1) {
+        lahendatud_viiteid ++
+        linked = true
+        i1.mviide.push(i2.memento)
+        return true // break out of isikud.some()
+      }
+    })
+    return !linked
+  })
+
+  // Lingin kokku, kus "vt " klapib ja klapivad ka sünniaastad
+  .filter(function(i1){
+    if (pass1) {
+      console.log('Kokku viiteid:', viiteid)
+      console.log('Esimesel ringil seotud viiteid:', lahendatud_viiteid)
+      pass1 = false
     }
-    isik.kasMitteküüditatud = match ? "1" : "0"
-  })(isik)
+    let linked = false
+    let a = i1['sünniaasta'] ? i1['sünniaasta'] : false
+    if (!a || a === '0000') { return true } // Vaatlen järgmisel ringil
+    let q1 = i1.kasutamataKirjeosa
+    isikud.some(function(i2){
+      if (i2.memento === '') { return false }
+      if (q1.indexOf(i2.eesnimi) !== -1
+      && q1.indexOf(i2.perenimi) !== -1
+      && a
+      && a !== '0000'
+      && (a === i2['sünniaasta'] || a === i2['altSünd']) ) {
+        lahendatud_viiteid ++
+        linked = true
+        i1.mviide.push(i2.memento)
+        return true // break out of isikud.some()
+      }
+    })
+    return !linked
+  })
 
-  // saatus teadmata
-  ;((isik) => {
-    let re = /\b[a-zA-Z]*(ine saatus teadmata|aatus teadmata)/
-    let match = re.exec(isik.kasutamataKirjeosa)
-    if (match !== null) {
-      isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@SAATUSTEADMATA@')
+  // Lingin kokku, kus "vt " klapib ja viidataval on kasutamataKirjeosa "ka "
+  .filter(function(i1){
+    if (pass2) {
+      console.log('Peale teist ringi on seotud viiteid:', lahendatud_viiteid)
+      pass2 = false
     }
-    isik.kasSaatusTeadmata = match ? "1" : "0"
-  })(isik)
+    let linked = false
+    let q1 = i1.kasutamataKirjeosa
+    isikud.some(function(i2){
+      if (i2.memento === '') { return false }
+      if (i2.kasutamataKirjeosa === '') { return false }
+      let q2 = i2.kasutamataKirjeosa
+      if (q2.indexOf('ka ') === -1) { return false }
+      if (q1.indexOf(i2.eesnimi) !== -1
+      && q1.indexOf(i2.perenimi) !== -1 ) {
+        lahendatud_viiteid ++
+        linked = true
+        i1.mviide.push(i2.memento)
+        return true // break out of isikud.some()
+      }
+    })
+    return !linked
+  })
 
-  // surmaotsus
-  ;((isik) => {
-    let re = /([Ss]urmaotsus\.?)/
-    let match = re.exec(isik.kasutamataKirjeosa)
-    if (match !== null) {
-      isik['surmaotsus'] = '+'
-      isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@SURMAOTSUS@')
-    }
-  })(isik)
+  // miskit jäi üle ka
+  .forEach(function(i1) {
+    console.log(i1)
+  })
 
-  // Sünd
-  ;((isik) => {
-    let re = /\b[Ss]\. ([0-9\.]*)/
-    let match = re.exec(isik.kasutamataKirjeosa)
-    isik['sünniaasta'] = match ? guessDate(match[1])[0].slice(0,4) : '0000'
-    if (match !== null) {
-      isik['sünd'] = guessDate(match[1])[0]
-      isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@SÜND@')
-    }
-  })(isik)
-
-  // Hukkunud isikud
-  ;((isik) => {
-    let re = /(((otsus t(ä|a\u0308)ide viidud|m[õõ]rv[a\.]|\b[Ss]urn|\b[Ss]uri\b|tapetud)[a-zA-ZÕÜÄÖõüäö \-\.]*)([\.0123456789]*))/
-    let match = re.exec(isik.kasutamataKirjeosa)
-    if (match !== null) {
-      // hukkunud.push(isik)
-      isik.hukkunud = guessDate(match[5])[0]
-      isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@HUKKUNUD@')
-    }
-    isik.kasHukkunud = isik.hukkunud ? "1" : "0"
-  })(isik)
-
-  // Küüditamised
-  ;((isik) => {
-    // paraku saab ü tähte esitada enam kui ühel moel
-    let re = /(k(ü|u\u0308){1,2}dit(\.|ati)) +([\.0123456789]*)/g
-    while (match = re.exec(isik.kasutamataKirjeosa)) {
-      if (!isik['küüditamine']) { isik['küüditamine'] = [] }
-      isik['küüditamine'].push(guessDate(match[4])[0])
-    }
-    if (isik['küüditamine']) isik['küüditamine'] = isik['küüditamine'].join('; ')
-    isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@KÜÜDITATUD@')
-  })(isik)
-
-  // Arreteerimised
-  ;((isik) => {
-    let re = /(arr\.) ?([\.0123456789]*)/g
-    while (match = re.exec(isik.kasutamataKirjeosa)) {
-      if (!isik.arreteerimine) { isik.arreteerimine = [] }
-      isik.arreteerimine.push(guessDate(match[2])[0])
-    }
-    if (isik.arreteerimine) isik.arreteerimine = isik.arreteerimine.join('; ')
-    isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@ARRETEERITUD@')
-  })(isik)
-
-  // Vabanemised
-  ;((isik) => {
-    let re = /(vab\. asum\.|vab\.) ?([\.0123456789]*)/g
-    while (match = re.exec(isik.kasutamataKirjeosa)) {
-      if (!isik.vabanemine) { isik.vabanemine = [] }
-      isik.vabanemine.push(guessDate(match[2])[0])
-    }
-    isik.kasVabanenud = isik.vabanemine ? "1" : "0"
-    if (isik.vabanemine) {
-      isik.vabanemine = isik.vabanemine.join('; ')
-    }
-    isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@VABANENUD@')
-  })(isik)
-
-  // Allikaviited
-  ;((isik) => {
-    let re = /\[(.*)\]/
-    let match = re.exec(isik.kasutamataKirjeosa)
-    if (match !== null) {
-      isik.allikad = match[1].split(',').map(function(a){return a.trim()})
-      isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@ALLIKAD@')
-    }
-    if (isik.allikad) isik.allikad = isik.allikad.join('; ')
-  })(isik)
-
-  // Sünd 2.
-  ;((isik) => {
-    let re = /@NIMI@([^0-9]+)([0-9][0-9\.]+)/
-    let match = re.exec(isik.kasutamataKirjeosa)
-    if (match !== null) {
-      // isik['sünd'] = match
-      isik['sünd'] = guessDate(match[2])[0]
-      isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@NIMI@$1@SÜND@')
-    }
-  })(isik)
-
-  // Haridus
-  ;((isik) => {
-    let re = /([1-9]{1,2}) ?kl[\.,;]* ?/g
-    while (match = re.exec(isik.kasutamataKirjeosa)) {
-      if (isik.haridus) { isik.haridus += (', ' + match[1] + ' kl.') }
-      else { isik.haridus = match[1] + ' kl.' }
-    }
-    isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@HARIDUS@')
-  })(isik)
-  ;((isik) => {
-    let re = /(kõrgh|kõrgh|keskh)(ar){0,1}[\., ]*/g
-    while (match = re.exec(isik.kasutamataKirjeosa)) {
-      if (isik.haridus) { isik.haridus += ', ' + match[1] + '.' }
-      else { isik.haridus = match[1] + '.' }
-    }
-    isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@HARIDUS@')
-  })(isik)
-
-  // Erinõukogu otsus ja paragraf
-  ;((isik) => {
-    let re = /[Ee]rin\. *([0-9\.]{4,8}) *§ *((?:[0-9\-]+[, ]*)+)([a-zA-Z]+)/g
-    while (match = re.exec(isik.kasutamataKirjeosa)) {
-      if (!isik.erin) { isik.erin = [] }
-      isik.erin.push({
-        otsus: guessDate(match[1])[0],
-        paragrahv: match[2]
-      })
-    }
-    isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '@ERIN@ $3')
-  })(isik)
-
-  // Nimekujud
-  ;((isik) => {
-    let re = /(@NIMI@[, ;\.]*(?:@SUGULUS@[, ;\.]*)*)((?:(?:in|en)\.?)? ?ka[^@]*)((?:@RAHVUS@[, ;\.]*)*(?:@SÜND@[, ;\.]*)*)/
-    let match = re.exec(isik.kasutamataKirjeosa)
-    if (match !== null) {
-      isik['nimekujud'] = match[2]
-      isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '$1 @NIMEKUJU@ $3')
-    }
-  })(isik)
-
-
-  // Liigsed märgid teekide vahel
-  ;((isik) => {
-    let re = /([A-ZÕÜÄÖ]@)[, ;\.]*(@[A-ZÕÜÄÖ])/g
-    isik.kasutamataKirjeosa = isik.kasutamataKirjeosa.replace(re, '$1 $2')
-  })(isik)
-
-
-  csvWrite(isik)
-})
-
-// let hukkunudY = YAML.stringify(hukkunud, 3, 4)
-// fs.writeFileSync('hukkunud.yaml', hukkunudY)
-
-// let isikudY = YAML.stringify(isikud, 3, 2)
-// fs.writeFileSync('rich_memento.yaml', isikudY)
-// logger.log('isikud')
-// logger.log(isikud)
-// logger.log('posikud')
+  console.log('viiteid ' + lahendatud_viiteid + '/' + viiteid);
+  isikud.forEach(function(isik){
+    csvWrite(isik)
+  })
+}
