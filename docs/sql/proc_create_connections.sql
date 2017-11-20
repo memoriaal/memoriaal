ca188fb958b59ad389bad6542b2adee74f29b5bb
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2017-11-21 01:08:08 +0200

    m-n connections

--- docs/sql/proc_create_connections.sql
+++ docs/sql/proc_create_connections.sql
@@ -2,6 +2,25 @@ DELIMITER ;;
 CREATE or replace PROCEDURE `create_connections`(IN ik1 CHAR(10), IN seoseliik VARCHAR(50), IN ik2 CHAR(10))
 BEGIN
     DECLARE msg VARCHAR(200);
+    DECLARE _ik1 CHAR(10);
+    DECLARE _ik2 CHAR(10);
+    DECLARE finished INTEGER DEFAULT 0;
+
+    DECLARE cur1 CURSOR FOR
+        SELECT n_ik1, n_ik2
+        FROM
+          (SELECT isikukood2 AS n_ik1
+           FROM seosed
+           WHERE isikukood1 = ik1
+             AND seos = 'sama isik'
+           UNION ALL SELECT ik1) j1
+        LEFT JOIN
+          (SELECT isikukood2 AS n_ik2
+           FROM seosed
+           WHERE isikukood1 = ik2
+             AND seos = 'sama isik'
+           UNION ALL SELECT ik2) j2 ON 1=1 ;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
 
     IF ik1 = ik2 THEN
         SELECT CONCAT( 'Ei hakka iseenda vahel seost looma, nuh.' ) INTO msg;
@@ -12,23 +31,33 @@ BEGIN
         SET seoseliik = 'sama isik';
     END IF;
 
-    IF seoseliik = 'sama isik' THEN
-        -- CALL validate_checklist(ik1, ik2);
-        INSERT INTO `z_queue` (`isikukood1`, `isikukood2`, `task`, `params`)
-        VALUES (ik1, ik2, 'propagate checklist', '');
+    IF seoseliik = 'sama isik' OR seoseliik = 'kahtlusseos' OR seoseliik = 'abikaasa' THEN
+        SET @vastasseos = seoseliik;
+    ELSE
+        SET @vastasseos = NULL;
     END IF;
 
-    IF seoseliik = 'sama isik' OR seoseliik = 'kahtlusseos' OR seoseliik = 'abikaasa' THEN
+    OPEN cur1;
+    read_loop: LOOP
+        FETCH cur1 INTO _ik1, _ik2;
+        IF finished = 1 THEN
+            LEAVE read_loop;
+        END IF;
+
+        IF seoseliik = 'sama isik' THEN
+            -- CALL validate_checklist(ik1, ik2);
+            INSERT INTO `z_queue` (`isikukood1`, `isikukood2`, `task`, `params`)
+            VALUES (_ik1, _ik2, 'propagate checklist', '');
+        END IF;
+
         INSERT IGNORE INTO seosed
-            SET isikukood1 = ik1, seos = seoseliik, vastasseos = seoseliik, isikukood2 = ik2;
+            SET isikukood1 = _ik1, seos = seoseliik, vastasseos = @vastasseos, isikukood2 = _ik2;
         INSERT IGNORE INTO seosed
-            SET isikukood1 = ik2, seos = seoseliik, vastasseos = seoseliik, isikukood2 = ik1;
-    ELSE
-        INSERT IGNORE INTO seosed
-            SET isikukood1 = ik1, seos = seoseliik, isikukood2 = ik2;
-        INSERT IGNORE INTO seosed
-            SET isikukood1 = ik2, vastasseos = seoseliik, isikukood2 = ik1;
-    END IF;
+            SET isikukood1 = _ik2, seos = @vastasseos, vastasseos = seoseliik, isikukood2 = _ik1;
+
+    END LOOP;
+    CLOSE cur1;
+    SET finished = 0;
 
 END;;
 DELIMITER ;
