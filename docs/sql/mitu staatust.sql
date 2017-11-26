64247ee31f7cf615017a3c7252a1950c33ffa2b2
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2017-11-26 13:36:36 +0200

    move

--- docs/sql/mitu staatust.sql
+++
@@ -1,9 +0,0 @@
-SELECT a.`id`, a.`sünniaasta`, a.`perenimi`, a.`eesnimi`, a.`isanimi`, a.`unikaalsus`, a.`dubleerib`
-, trim(group_concat(a.`sünniaeg` SEPARATOR ' ')) as sünniaeg, '' as surmaaeg, trim(group_concat(a.`sünnikoht` SEPARATOR ' ')) as sünnikoht, trim(group_concat(a.`elukoht` SEPARATOR ' ')) as elukoht
-, st.kashukkunud, st.kasvabanenud, st.kasmitteküüditatud
-, trim(group_concat(a.`tapetud` SEPARATOR ' ')) as `tapetud (AAAA-KK-PP)`, trim(group_concat(a.`küüditatud` SEPARATOR ' ')) as `küüditatud (AAAA-KK-PP)`, trim(group_concat(a.`arreteeritud` SEPARATOR ' ')) as `arreteeritud (AAAA-KK-PP)`
-, trim(replace(group_concat(a.allikas, '; VHM:', a.kasvabanenud, a.kashukkunud, a.kasmitteküüditatud, '; ', a.kirje SEPARATOR "\n"),'"', '''')) as kirjed
-FROM staatused_v st
-LEFT JOIN repr_v a on a.id = st.id
-WHERE st.kashukkunud + st.kasvabanenud + st.kasmitteküüditatud > 1
-GROUP BY `id`, `sünniaasta`, `perenimi`, `eesnimi`, `isanimi`, `unikaalsus`, `dubleerib`, st.kashukkunud, st.kasvabanenud, st.kasmitteküüditatud
--- docs/sql/proc_remove_connection.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER ;;
-CREATE OR REPLACE PROCEDURE remove_connection(IN ik1 CHAR(10), IN ik2 CHAR(10))
-BEGIN
-    DELETE FROM seosed
-    WHERE isikukood1 = ik1 AND isikukood2 = ik2;
-    DELETE FROM seosed
-    WHERE isikukood1 = ik2 AND isikukood2 = ik1;
-END;;
-DELIMITER ;
